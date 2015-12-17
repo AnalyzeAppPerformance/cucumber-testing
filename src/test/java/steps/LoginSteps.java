e3ad7d2353c8c159b58a3b7d48140bfbd9927422
panczur <panczur@gmail.com>
2015-12-17 15:50:35 +0100

    some new scenarios added

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -13,12 +14,13 @@ public class LoginSteps {
     private HomePageActions homePageActions;
     private LoginPageActions loginPageActions;
 
+
     public LoginSteps() {
         this.homePageActions = new HomePageActions();
         this.loginPageActions = new LoginPageActions();
     }
 
-    @Given("^I am on login page$")
+    @And("^I am on login page$")
     public void I_am_on_login_page() throws Throwable {
         homePageActions.openHomePage();
         homePageActions.goToLoginPage();
@@ -32,5 +34,27 @@ public class LoginSteps {
     @Then("^I should be signed in$")
     public void I_should_be_signed_in() throws Throwable {
         Assert.assertTrue(homePageActions.isLogoutButton());
+    }
+
+    @Given("^I am logged into the system$")
+    public void iAmLoggedIntoTheSystem() throws Throwable {
+        I_am_on_login_page();
+        loginPageActions.signInUser("artur", "123456");
+    }
+
+    @When("^I logout from system$")
+    public void iLogoutFromSystem() throws Throwable {
+        homePageActions.logoutFromSystem();
+    }
+
+    @Then("^I am logged out$")
+    public void iAmLoggedOut() throws Throwable {
+        Assert.assertTrue(homePageActions.isUserNotLogged());
+    }
+
+    @Given("^I am logged into the system as user \"([^\"]*)\" with password \"([^\"]*)\"$")
+    public void iAmLoggedIntoTheSystemAsUserWithPassword(String login, String password) throws Throwable {
+        I_am_on_login_page();
+        loginPageActions.signInUser(login, password);
     }
 }
